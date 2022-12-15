228fdeb23033d86ebe9ac92b1fd6353333db8557
OsamaNaeem <35254270+OsamaNaeem@users.noreply.github.com>
2022-12-14 21:19:12 -0500

    Add files via upload

---
+++ ChatGP3.1/ChatGP3.1/ChatGP3_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ChatGP3_1App.swift
+//  ChatGP3.1
+//
+//  Created by Muhammad Osama Naeem on 12/14/22.
+//
+
+import SwiftUI
+
+@main
+struct ChatGP3_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatGP3.1/ChatGP3.1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ChatGP3.1
+//
+//  Created by Muhammad Osama Naeem on 12/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
